4149d9e435f445e55f89cc70d80fa03308c08e9c
olisabelema <olisabelema@gmail.com>
2023-02-18 07:32:17 -0800

    100-print_comb3 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 48;
+
+	while (n < 58)
+	{
+	m = 48;
+	while (m < 58)
+	{
+		putchar(n);
+		putchar(m);
+		m++;
+
+		putchar(44);
+		putchar(32);
+	}
+	n++;
+	}
+	putchar('\n');
+
+	return (0);
+	}
