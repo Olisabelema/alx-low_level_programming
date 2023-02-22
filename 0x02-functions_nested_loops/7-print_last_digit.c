25158ae649be36f317ff95dc48955ab3bdce77dd
olisabelema <olisabelema@gmail.com>
2023-02-22 01:03:22 -0800

    7-print_last_digit.c updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,11 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
+	num = num % 10;
+
 	if (num < 0)
 	{
 	num = num * -1;
-	last_digit = num % 10;
-	}
-	else
-	{
 	last_digit = num % 10;
 	}
 	_putchar('0' + last_digit);
