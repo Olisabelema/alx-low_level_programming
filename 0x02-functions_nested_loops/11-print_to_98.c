dae30029ce35e7079adcef978a82e0fb19fa2ced
olisabelema <olisabelema@gmail.com>
2023-02-22 04:06:25 -0800

    11-print_to_98.c done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,21 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-	for (; n > 98; n--)
+	for (; n >= 98; n--)
 	{
-	putchar(n);
+	printf("%d, ", n);
 	}
 	}
 	else if (n < 98)
 	{
-	for (; n < 98; n++)
+	for (; n <= 98; n++)
 	{
-	putchar(n);
+	printf("%d, ", n);
 	}
 	}
-	putchar(n);
+	else
+	{
+	printf("%d", n);
+	}
+	printf("%d\n", n);
 }
