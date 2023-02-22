c48b82c0673923a87bb64c7129315feb1eb88725
olisabelema <olisabelema@gmail.com>
2023-02-22 00:27:35 -0800

    7-print_last_digit.c update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,14 @@
 * @num:number to compute last digit.
 * Return:last digit.
 */
-
 int print_last_digit(int num)
 {
 	int last_digit;
 
 	if (num < 0)
+	num = num * -1;
 	{
-	last_digit = num % -10;
+	last_digit = num % 10;
 	}
 	else
 	{
