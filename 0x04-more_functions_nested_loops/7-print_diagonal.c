0499e1af80d47417cfb0366f101ca8c71ac2230f
olisabelema <olisabelema@gmail.com>
2023-02-23 15:13:57 -0800

    7-print_diagonal.c updated

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit of a number.
+ *@l: The character to print
+ * Return: Always l.
+ */
+int print_last_digit(int l)
+{
+	l = l % 10;
+
+	if (l < 0)
+	l *= -1;
+
+	_putchar(l + '0');
+
+	return (l);
+
+}
