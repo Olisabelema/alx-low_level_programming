b676cffc811a10578aefbf3d42e8460374aa4d2e
olisabelema <olisabelema@gmail.com>
2023-03-14 02:21:04 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,12 +11,10 @@ int count_word(char *s)
 
 	flag = 0;
 	w = 0;
-
 	for (c = 0; s[c] != '\0'; c++)
 	{
 	if (s[c] == ' ')
 	flag = 0;
-
 	else if (flag == 0)
 	{
 	flag = 1;
@@ -41,12 +39,10 @@ char **strtow(char *str)
 
 	words = count_word(str);
 	if (words == 0)
-
 	return (NULL);
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
-
 	return (NULL);
 
 	for (i = 0; i <= len; i++)
@@ -56,17 +52,14 @@ char **strtow(char *str)
 	if (c)
 	{
 	end = i;
-
 	tmp = (char *) malloc(sizeof(char) * (c + 1));
 
 	if (tmp == NULL)
-
 	return (NULL);
 
 	while (start < end)
 	*tmp++ = str[start++];
 	*tmp = '\0';
-
 	matrix[k] = tmp - c;
 	k++;
 	c = 0;
@@ -76,6 +69,5 @@ char **strtow(char *str)
 	start = i;
 	}
 	matrix[k] = NULL;
-
 	return (matrix);
 	}
