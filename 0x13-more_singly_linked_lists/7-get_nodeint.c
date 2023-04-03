a2a2f9da6a16f0d1416bc12a91160fc92874559c
olisabelema <olisabelema@gmail.com>
2023-04-03 10:38:19 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,9 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	for (unsigned int i = 0; head != NULL; i++, head = head->next)
+	unsigned int i = 0;
+
+	for (; head != NULL; i++, head = head->next)
 		{
 		if (i == index)
 			{
