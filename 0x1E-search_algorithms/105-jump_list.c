426a3e8edd866dd472df8d700df3c31828b28133
olisabelema <olisabelema@gmail.com>
2023-09-13 23:49:29 -0700

    0-linear.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,38 +1,25 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_list - jump list in a singly linked list
- * @list: pointer to start of list
- * @size: number of nodes in the list
+ * linear_search - searches for a value in an array of integers
+ *		using the Linear search algorithm
+ * @array: array to search
+ * @size: size of array
  * @value: value to be searched for
  *
- * Return: index of value found, otherwise NULL
+ * Return: index of found value, -1 if arr is null or not found
  */
-listint_t *jump_list(listint_t *list, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	size_t block_index = 0, num_blocks_passed = 0, block_size = 0;
-	listint_t *prev = list;
+	size_t i;
 
-	if (list == NULL || size == 0)
-		return (NULL);
-	block_size = sqrt((double)size);
-	do {
-		prev = list;
-		num_blocks_passed++;
-		block_index = block_size * num_blocks_passed;
-		while (list->next && list->index < block_index)
-			list = list->next;
-		printf("Value checked at index [%d] = [%d]\n", (int)block_index, list->n);
-	} while (block_index < size && list->n < value && list->next);
-	printf("Value found between indexes [%d] and [%d]\n",
-			(int)prev->index, (int)list->index);
-	while (prev && prev->index <= list->index)
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
-	return (NULL);
+	return (-1);
 }
