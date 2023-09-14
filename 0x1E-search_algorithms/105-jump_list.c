6e9f325189d0a08c53e6575380950357f065df09
olisabelema <olisabelema@gmail.com>
2023-09-14 02:48:53 -0700

    105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,25 +1,60 @@
+#include <math.h>
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers
- *		using the Linear search algorithm
- * @array: array to search
- * @size: size of array
- * @value: value to be searched for
+ * get_n_next - Gets the nth node next to a given node.
+ * @node: starting node.
+ * @n: number of positions next to the node.
  *
- * Return: index of found value, -1 if arr is null or not found
+ * Return: The node n places next to the given node,
+ * otherwise the last node or NULL.
  */
-int linear_search(int *array, size_t size, int value)
+listint_t *get_n_next(listint_t *node, size_t n)
 {
-	size_t i;
+	size_t index = 0;
+	listint_t *res = NULL;
 
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	res = node;
+	for (index = 0; (index < n) && (res) && (res->next); index++)
+		res = res->next;
+	return (res);
+}
+
+/**
+ * jump_list - Searches a value in a sorted linked list using a jump search.
+ * @list: linked list to search in.
+ * @size: length of the linked list.
+ * @value: value to look for.
+ *
+ * Return: node with the value in the linked list, otherwise NULL.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, a = 0, b = 0;
+	listint_t *node, *next;
+
+	if (!list)
+		return (NULL);
+	step = (size_t)sqrt(size);
+	node = list;
+	next = get_n_next(node, step);
+	while (node)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked at index [%d] = [%d]\n", (int)next->index, next->n);
+		if ((next->n >= value) || (!next->next))
+			break;
+		node = next;
+		next = get_n_next(node, step);
 	}
-	return (-1);
+	a = node->index;
+	b = next->index;
+	printf("Value found between indexes [%d] and [%d]\n", (int)a, (int)b);
+	while (node)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)node->index, node->n);
+		if (node->n == value)
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
 }
